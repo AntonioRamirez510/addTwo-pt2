a8316930263098b602d27709ce15c8842e3646a5
Antonio Ramirez <antonioramirez9614@gmail.com>
2024-07-09 10:07:32 -0500

    done with stretch

---
+++ index.js
@@ -0,0 +1,8 @@
+const addTwo = (nums) => nums.map((x) => x+2);
+
+const numbers = [8,18,28,38,48];
+console.log(`const addTwo = (nums) => nums.map((x) => x+2);`)
+console.log(`const numbers = [8,18,28,38,48];`)
+console.log(`addTwo(numbers): ${addTwo(numbers)}`);
+
+
